5e5574c55bdc6eee96813c5e98f40132e442325d
digitalfabric92 <dwawryko@gmail.com>
2017-11-19 12:55:43 -0500

    Updated Map

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,4 +1,19 @@
+// UGLY CONSOLE LOGS
+console.log("It ran: ")
+console.log("Property name: ", getElementById('property-type'))
+
 function initMap() {
+
+
+    // Assassin's creed
+    var styledMapType = new google.maps.StyledMapType(
+        [{"featureType":"all","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"all","elementType":"labels","stylers":[{"visibility":"off"},{"saturation":"-100"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40},{"visibility":"off"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"lightness":21}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"color":"#4d6059"}]},{"featureType":"poi","elementType":"geometry.stroke","stylers":[{"color":"#4d6059"}]},{"featureType":"road","elementType":"geometry","stylers":[{"visibility":"on"},{"color":"#7f8d89"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#7f8d89"}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#7f8d89"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#2b3638"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#2b3638"},{"lightness":17}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"geometry.stroke","stylers":[{"color":"#24282b"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels.icon","stylers":[{"visibility":"off"}]}],{name: 'Styled Map'});
+
+    // Marauder's map
+    var styledMapType = new google.maps.StyledMapType(
+        [{"featureType":"all","elementType":"all","stylers":[{"color":"#4b0202"},{"gamma":"2.38"},{"saturation":"0"},{"visibility":"simplified"}]},{"featureType":"all","elementType":"geometry","stylers":[{"color":"#ebc876"}]},{"featureType":"all","elementType":"labels.text.fill","stylers":[{"gamma":0.01},{"lightness":20}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"saturation":-31},{"lightness":-33},{"weight":2},{"gamma":0.8}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"all","stylers":[{"color":"#a00404"},{"weight":"0.18"}]},{"featureType":"administrative","elementType":"labels","stylers":[{"color":"#980000"}]},{"featureType":"administrative.country","elementType":"all","stylers":[{"color":"#690000"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"color":"#950000"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"color":"#4b0202"}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"lightness":30},{"saturation":30}]},{"featureType":"poi","elementType":"geometry","stylers":[{"saturation":20}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"lightness":20},{"saturation":-20}]},{"featureType":"road","elementType":"all","stylers":[{"color":"#fff0bc"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":10},{"saturation":-30}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"saturation":25},{"lightness":25}]},{"featureType":"transit.line","elementType":"all","stylers":[{"color":"#4b0202"},{"weight":"0.50"}]},{"featureType":"water","elementType":"all","stylers":[{"lightness":-20}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#d6a95d"}]}],{name: 'Styled Map'});
+
+
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
         center: {lat: 45.5008033, lng: -73.5747155}
@@ -8,21 +23,26 @@ function initMap() {
     document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
+
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
 }
 
-var contentString = '<p>Suggested Price: $150 CAD</p>'
+// var contentString = `<p>Suggested Price: "${price}" CAD</p>`
 
-function geocodeAddress(geocoder, resultsMap) {
+function geocodeAddress(geocoder, styledMap) {
     var address = document.getElementById('address').value;
     geocoder.geocode({'address': address}, function(results, status) {
         if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
+            styledMap.setCenter(results[0].geometry.location);
             var marker = new google.maps.Marker({
-                map: resultsMap,
+                map: styledMap,
                 position: results[0].geometry.location
             });
             var infowindow = new google.maps.InfoWindow({
-                content: contentString
+                // content: contentString;
+                content: '<p>Suggested Price: $150 CAD</p>'
             });
             marker.addListener('click', function() {
                 infowindow.open(map, marker);
@@ -34,7 +54,6 @@ function geocodeAddress(geocoder, resultsMap) {
             alert('Geocode was not successful for the following reason: ' + status);
         }
     });
-
 
 }
 
